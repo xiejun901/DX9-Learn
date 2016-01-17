11e706dcdf7fcc4dbd2bb8fb3af28547bc33b9e1
xiejun901 <xiejun901@gmail.com>
2016-01-17 10:43:13 +0800

    删除了点多余代码

---
+++ DX9-Leran/TeaPotWindow.h
@@ -0,0 +1,64 @@
+#ifndef _CUBE_WINDOW_H_
+#define _CUBE_WINDOW_H_
+#include "d3dMainWindow.h"
+
+class TeaPotWindow :
+    public D3DMainWindow
+{
+public:
+    TeaPotWindow(const wchar_t * _name, int _width, int _height):D3DMainWindow(_name, _width, _height){}
+    void Setup() override 
+    {
+        D3DXCreateTeapot(pd3dDevice, &Teapot, 0);
+
+        D3DXVECTOR3 position(0.0f, 0.0f, -3.0f);
+        D3DXVECTOR3 target(0.0f, 0.0f, 0.0f);
+        D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);
+        D3DXMATRIX v;
+        D3DXMatrixLookAtLH(&v, &position, &target, &up);
+        pd3dDevice->SetTransform(D3DTS_VIEW, &v);
+        D3DXMATRIX proj;
+        D3DXMatrixPerspectiveFovLH(
+            &proj,
+            D3DX_PI * 0.5f,
+            (float)width / (float)height,
+            1.0f,
+            1000.0f
+            );
+        pd3dDevice->SetTransform(D3DTS_PROJECTION, &proj);
+
+        pd3dDevice->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);
+        pd3dDevice->SetRenderState(D3DRS_LIGHTING, FALSE);
+        pd3dDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
+
+    }
+    bool Display(float timeDelta) override
+    {
+        if (pd3dDevice)
+        {
+            pd3dDevice->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_COLORVALUE(0.0f, 1.0f, 0.0f, 1.0f), 1.0f, 0);
+            pd3dDevice->BeginScene();
+            D3DXMATRIX  ry;
+            static float y = 0.0f;
+            D3DXMatrixRotationY(&ry, y);
+            y += timeDelta;
+            if (y >= 6.28f)
+            {
+                y = 0.0f;
+            }
+            pd3dDevice->SetTransform(D3DTS_WORLD, &ry);
+
+            Teapot->DrawSubset(0);
+
+            pd3dDevice->EndScene();
+            pd3dDevice->Present(0, 0, 0, 0);
+        }
+        return true;
+    }
+private:
+    IDirect3DVertexBuffer9 *vb = nullptr;
+    IDirect3DIndexBuffer9 *ib = nullptr;
+    ID3DXMesh *Teapot = nullptr;
+};
+#endif
+
--- DX9-Leran/main.cpp
+++ DX9-Leran/main.cpp
@@ -1,14 +1,14 @@
 #include<windows.h>
 #include<d3dx9.h>
 #include"d3dmainWindow.h"
-#include "CubeWindow.h"
+#include "TeapotWindow.h"
 
 int WINAPI WinMain(HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
     LPSTR     lpCmdLine,
     int       nCmdShow)
 {
-    CubeWindow app(L"WINDOW", 640, 480);
+    TeaPotWindow app(L"WINDOW", 640, 480);
     app.Init();
     app.Setup();
     app.run();
